61e3ff83d0f96b86dfe0aa4e0a1e09a71559b8bf
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-07-04 09:53:51 -0700

    paddles move

--- Pong1.py
+++ Pong1.py
@@ -13,7 +13,7 @@ paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350, 0)
+paddle_a.goto(-350, 0) # Sets paddle to the right side
 
 # Paddle B
 paddle_b = turtle.Turtle()
@@ -22,7 +22,7 @@ paddle_b.color("white")
 paddle_b.shape("square")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350, 0)
+paddle_b.goto(350, 0) # Sets paddle to the left side
 
 # Ball 
 ball = turtle.Turtle()
@@ -30,7 +30,35 @@ ball.speed(0)
 ball.color("white")
 ball.shape("square")
 ball.penup()
-ball.goto(0, 0)
+ball.goto(0, 0) # Sets ball in the middle
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor() # Returns the y coordinate
+    y += 20 # Add 20 px to the y coord
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor() 
+    y -= 20 
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor() 
+    y += 20 
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor() 
+    y -= 20 
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen() # Tells it to listen for keyboard input
+wn.onkeypress(paddle_a_up, "w") # When the user presses w, call the function paddle_a_up
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up") # When the user presses w, call the function paddle_a_up
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
