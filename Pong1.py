1cd487bf984c7f8c33ecb925ff098e289cbfebf8
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-07-04 10:14:51 -0700

    paddle and ball collision working

--- Pong1.py
+++ Pong1.py
@@ -86,3 +86,12 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+    # Paddle and ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
