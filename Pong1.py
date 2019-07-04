4931958c9e4cfd84a5b4940f06320f9828526400
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-07-04 09:44:45 -0700

    paddle and ball created

---
+++ Pong1.py
@@ -0,0 +1,37 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.color("white")
+paddle_b.shape("square")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball 
+ball = turtle.Turtle()
+ball.speed(0)
+ball.color("white")
+ball.shape("square")
+ball.penup()
+ball.goto(0, 0)
+
+# Main game loop
+while True:
+    wn.update()
