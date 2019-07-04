06d0b1470daaff8b8cd72d57ba909d2bf68b8806
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-07-04 10:42:01 -0700

    sound added on collision, game complete

--- Pong1.py
+++ Pong1.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 wn = turtle.Screen()
 wn.title("Pong")
@@ -87,10 +88,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1 # Reverses the direction
+        os.system("afplay bounce.wav&")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1 
+        os.system("afplay bounce.wav&")
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -110,7 +113,9 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
+        os.system("afplay bounce.wav&")
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+        os.system("afplay bounce.wav&")
